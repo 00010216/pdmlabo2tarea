827024266a3009b10ee5036d691f3cfc11d40e50
00010216 <00010216@uca.edu.sv>
2018-03-23 17:15:29 -0600

    fourth commit

--- app/src/main/java/com/example/kcruz/tennismatch/MainActivity.java
+++ app/src/main/java/com/example/kcruz/tennismatch/MainActivity.java
@@ -8,51 +8,79 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button btn1, btn1_;
-    private TextView txtF, txtM;
+    private Button btn1, btn2, btn_reset;
+    private TextView txtF, txtM,txtD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn1 = findViewById(R.id.btnF_15);
+        txtD = findViewById(R.id.deuce);
+        btn1 = findViewById(R.id.btnF);
         btn1.setOnClickListener(this);
-        btn1_ = findViewById(R.id.btnM_15);
-        btn1_.setOnClickListener(this);
+        btn2 = findViewById(R.id.btnM);
+        btn2.setOnClickListener(this);
+        btn_reset = findViewById(R.id.btn_reset);
+        btn_reset.setOnClickListener(this);
+
         txtF = findViewById(R.id.scoreFederer);
         txtF.setOnClickListener(this);
         txtM = findViewById(R.id.scoreMurray);
         txtM.setOnClickListener(this);
-
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-
-            case R.id.btnF_15:
-                txtF.setText("15");
+            case R.id.btnF:
+                scoringSystem(1);
                 break;
-
-            case R.id.btnF_30:
-                // do your code
+            case R.id.btnM:
+                scoringSystem(2);
                 break;
-
-            case R.id.btnF_40:
-                // do your code
-                break;
-
-            case R.id.btnM_15:
-                txtM.setText("15");
-                break;
-
-            /*case R.id.reset:
-                txtF.setText("0") & txtM.setText("0");
-                break;*/
+            case R.id.btn_reset:
+                txtF.setText("0");
+                txtM.setText("0");
+                txtD.setText("");
             default:
                 break;
         }
+    }
+
+    public void scoringSystem (int scored){
+        int pointsF = Integer.parseInt(txtF.getText().toString());
+        int pointsM = Integer.parseInt(txtM.getText().toString());
+        if(pointsF>=40&&pointsM>=40){
+            if(scored==1)pointsF+=5;
+            else if(scored==2)pointsM+=5;
+            if(pointsF-pointsM==10){
+                txtD.setText("Federer wins");
+            }
+            else if(pointsM-pointsF==10) {
+                txtM.setText("Murray wins");
+            }
+            else {
+                txtF.setText(pointsF+"");
+                txtM.setText(pointsM+"");
+            }
+        }
+        else{
+            if(scored==1){
+                if(pointsF==40)pointsF+=5;
+                else if(pointsF==30)pointsF+=10;
+                else pointsF+=15;
+            }
+            else if(scored==2){
+                if(pointsM==40)pointsM+=5;
+                else if(pointsM==30)pointsM+=10;
+                else pointsM+=15;
+            }
+            txtF.setText(pointsF+"");
+            txtM.setText(pointsM+"");
+        }
+        if (pointsF == pointsM && pointsF >= 40) txtD.setText("DEUCE");
+        else txtD.setText("");
 
     }
 }
